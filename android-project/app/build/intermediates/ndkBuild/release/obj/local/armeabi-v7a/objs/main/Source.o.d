7a9c9e2ef2449492c7ce6ed70fd865ac5e59ffb8
Anthony Dranfield <34162989+b6018718@users.noreply.github.com>
2018-07-22 21:02:06 +0100

    Initial Commit

---
+++ android-project/app/build/intermediates/ndkBuild/release/obj/local/armeabi-v7a/objs/main/Source.o.d
@@ -0,0 +1,2 @@
+C:/androidlib/SDL2-2.0.8/android-project/app/build/intermediates/ndkBuild/release/obj/local/armeabi-v7a/objs/main/Source.o: \
+  C:/androidlib/SDL2-2.0.8/android-project/app/jni/src/Source.cpp
---
+++ android-project/app/build/intermediates/ndkBuild/release/obj/local/x86/objs/main/Source.o.d
@@ -0,0 +1,2 @@
+C:/androidlib/SDL2-2.0.8/android-project/app/build/intermediates/ndkBuild/release/obj/local/x86/objs/main/Source.o: \
+  C:/androidlib/SDL2-2.0.8/android-project/app/jni/src/Source.cpp
